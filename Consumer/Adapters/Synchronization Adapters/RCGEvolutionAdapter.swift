c3356a740aad5eb4c5e56eda61d1a30921301ed4
redcirclegame <vladislavz1991@gmail.com>
2016-07-13 19:29:47 -0400

    update

---
+++ Consumer/Adapters/Synchronization Adapters/RCGEvolutionAdapter.swift
@@ -0,0 +1,11 @@
+//
+//  RCGEvolutionAdapter.swift
+//  Consumer
+//
+//  Created by Vlad Zagorodnyuk on 7/13/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+class RCGEvolutionAdapter: RCGSynchronizerAdapter {
+
+}
---
+++ Consumer/Adapters/Synchronization Adapters/RCGPokemonAdapter.swift
@@ -0,0 +1,11 @@
+//
+//  RCGPokemonAdapter.swift
+//  Consumer
+//
+//  Created by Vlad Zagorodnyuk on 7/13/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+class RCGPokemonAdapter: RCGSynchronizerAdapter {
+
+}
