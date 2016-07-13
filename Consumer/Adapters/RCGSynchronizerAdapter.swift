f17b8e7c6dbf816ecc01450676ac0c184f553466
redcirclegame <vladislavz1991@gmail.com>
2016-07-13 19:21:08 -0400

    adapters initial logic

---
+++ Consumer/Adapters/RCGSynchronizerAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  RCGSynchronizerAdapter.swift
+//  Consumer
+//
+//  Created by Vlad Zagorodnyuk on 7/13/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+class RCGSynchronizerAdapter: NSObject {
+
+}
