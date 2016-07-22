9dd110737deeafe1effe74713d24619566a02c2d
Vlad Z <vladislavz1991@gmail.com>
2016-07-22 13:37:26 -0400

    initial setup

---
+++ Consumer/Sections/RCGInitialViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RCGInitialViewController.swift
+//  Consumer
+//
+//  Created by Vladislav Zagorodnyuk on 7/22/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+class RCGInitialViewController: UIViewController {
+
+    
+}
