57faec1a04d54c0a1158f3749f3bdfa41ca01fa3
Vlad Z <vladislavz1991@gmail.com>
2016-07-22 13:27:27 -0400

    types

---
+++ Model/ModelHuman/RCGPokemonType.swift
@@ -0,0 +1,35 @@
+//
+//  RCGPokemonType.swift
+//  consumer
+//
+//  Created by Vladislav Zagorodnyuk on 2/3/16.
+//  Copyright Â© 2016 Red Circle Games. All rights reserved.
+//
+
+import CoreData
+import SwiftyJSON
+
+@objc(RCGPokemonType)
+public class RCGPokemonType: _RCGPokemonType {
+
+	// MARK: - Fetching logic
+	class func fetchObjectWithID(objectID: Int, context: NSManagedObjectContext) throws -> RCGPokemonType? {
+
+        return try RCGPokemonType.modelFetchWithID(objectID, context:context) as? RCGPokemonType
+    }
+
+    class func fetchOrInsertWithJSON(json: JSON, context: NSManagedObjectContext) throws -> RCGPokemonType? {
+
+        return try RCGPokemonType.modelFetchOrInsertWithJSON(json, context: context) as? RCGPokemonType
+    }
+
+    // MARK: - Parsing JSON
+    override func setValueWithJSON(json: JSON, context: NSManagedObjectContext) {
+
+	    super.setValueWithJSON(json, context: context)
+
+        if let name = json["name"].string {
+            self.name = name
+        }
+    }
+}
