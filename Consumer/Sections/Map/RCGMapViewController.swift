5206cefc3c65442405d88c47445a1e8f0eceb000
redcirclegame <vladislavz1991@gmail.com>
2016-07-26 21:39:13 -0400

    initial map controller

---
+++ Consumer/Sections/Map/RCGMapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RCGMapViewController.swift
+//  Consumer
+//
+//  Created by Vlad Zagorodnyuk on 7/26/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+class RCGMapViewController: UIViewController {
+
+    
+}
