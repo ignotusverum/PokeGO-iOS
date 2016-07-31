588889e7a772b270ac319d755271ede1d2e04647
Vlad Z <vladislavz1991@gmail.com>
2016-07-31 15:41:45 -0400

    updated models

---
+++ Model/ModelMachine/_RCGPokemonMap.swift
@@ -0,0 +1,68 @@
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to RCGPokemonMap.swift instead.
+
+import CoreData
+
+public enum RCGPokemonMapAttributes: String {
+    case disappearTime = "disappearTime"
+    case encounterID = "encounterID"
+    case latitude = "latitude"
+    case longitude = "longitude"
+    case spawnpointID = "spawnpointID"
+}
+
+@objc public
+class _RCGPokemonMap: RCGModel {
+
+    // MARK: - Class methods
+
+    override public class func entityName () -> String {
+        return "RCGPokemonMap"
+    }
+
+    override public class func entity(managedObjectContext: NSManagedObjectContext!) -> NSEntityDescription! {
+        return NSEntityDescription.entityForName(self.entityName(), inManagedObjectContext: managedObjectContext);
+    }
+
+    // MARK: - Life cycle methods
+
+    public override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext!) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+
+    public convenience init(managedObjectContext: NSManagedObjectContext!) {
+        let entity = _RCGPokemonMap.entity(managedObjectContext)
+        self.init(entity: entity, insertIntoManagedObjectContext: managedObjectContext)
+    }
+
+    // MARK: - Properties
+
+    @NSManaged public
+    var disappearTime: NSNumber?
+
+    // func validateDisappearTime(value: AutoreleasingUnsafeMutablePointer<AnyObject>, error: NSErrorPointer) -> Bool {}
+
+    @NSManaged public
+    var encounterID: String?
+
+    // func validateEncounterID(value: AutoreleasingUnsafeMutablePointer<AnyObject>, error: NSErrorPointer) -> Bool {}
+
+    @NSManaged public
+    var latitude: NSNumber?
+
+    // func validateLatitude(value: AutoreleasingUnsafeMutablePointer<AnyObject>, error: NSErrorPointer) -> Bool {}
+
+    @NSManaged public
+    var longitude: NSNumber?
+
+    // func validateLongitude(value: AutoreleasingUnsafeMutablePointer<AnyObject>, error: NSErrorPointer) -> Bool {}
+
+    @NSManaged public
+    var spawnpointID: String?
+
+    // func validateSpawnpointID(value: AutoreleasingUnsafeMutablePointer<AnyObject>, error: NSErrorPointer) -> Bool {}
+
+    // MARK: - Relationships
+
+}
+
